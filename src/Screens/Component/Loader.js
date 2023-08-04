0da0c7c21bc3d0eb6b186ca55c33390973fc27eb
someone <someone@someplace.com>
2023-08-05 00:15:24 +0400

    ff

---
+++ src/Screens/Component/Loader.js
@@ -0,0 +1,12 @@
+// import { ReactComponent as YourSvg } from '../../Assets/image/loader.svg';
+
+function Loader() {
+    return (
+        <>
+            <div className="loadingInkyPencil">
+                {/* <YourSvg /> */}
+            </div>
+        </>
+    )
+}
+export default Loader
